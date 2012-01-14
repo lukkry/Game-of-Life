820b3f1f6caa2afb894a5d0c894166c0bdd7cac8
Lukasz Krystkowiak <lukasz.krystkowiak@gmail.com>
2012-01-14 21:47:50 +0100

    README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Game of Life
+
+# Description
+World is considered as cylinder, so left edge is connect with right edge (that means that in the same row first and last cell are neighbours).
+
--- game_of_life.rb
+++ game_of_life.rb
@@ -1,6 +1,7 @@
 Dir[File.expand_path("lib/**/*.rb")].each {|f| require f}
 require 'ruby-prof'
 
-world = World.new(60, 30, nil, true)
+world = World.new(60, 30)
+world.randomize!
 100.times{world.tick!}
 
